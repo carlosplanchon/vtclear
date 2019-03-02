a0859175abb8fa101e0f11b19e9ba3095a6b45c4
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2019-03-02 18:21:25 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    name="VTClear",
+    packages=["VTClear"],
+    version="0.1",
+    license="GPL3",
+    description="Python3 ANSI VT100 implementation of Erase Screen.",
+    author="Carlos A. Planchón",
+    author_email="bubbledoloresuruguay2@gmail.com",
+    url="https://github.com/carlosplanchon/vtclear",
+    download_url="https://github.com/carlosplanchon/"
+        "reponame/archive/vtclear0.1.tar.gz",
+    keywords=["CLEAR", "VT100", "ANSI"],
+    classifiers=[
+        "Development Status :: 5 - Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: GPL3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
